d06ac9f24c8da20d3b3235e4d2403ccafa263954
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2021-01-26 17:07:26 +0200

    finshed the demo

--- app/src/main/java/com/sergway/bio/MainActivity.java
+++ app/src/main/java/com/sergway/bio/MainActivity.java
@@ -2,13 +2,30 @@ package com.sergway.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterHobbies;
+    private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterHobbies = findViewById(R.id.enterHobbies);
+        hobbies = findViewById(R.id.hobbiesText);
+    }
+
+    public void addHobbies(View view) {
+        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
+        hobbies.setVisibility(View.VISIBLE);
+
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
