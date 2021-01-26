80fd8c88f1ad6fca6424679f9862ff18730e1e5f
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2021-01-26 22:09:12 +0200

    add data binding

--- app/src/main/java/com/sergway/bio/MainActivity.java
+++ app/src/main/java/com/sergway/bio/MainActivity.java
@@ -1,30 +1,41 @@
 package com.sergway.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import com.sergway.bio.data.Bio;
+import com.sergway.bio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText enterHobbies;
-    private TextView hobbies;
+    private ActivityMainBinding binding;
+    private final Bio bio = new Bio();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        enterHobbies = findViewById(R.id.enterHobbies);
-        hobbies = findViewById(R.id.hobbiesText);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        bio.setName("Sergway Khodyachikh");
+
+        binding.setBio(bio);
+        binding.doneButton.setOnClickListener(this::addHobbies);
     }
 
     public void addHobbies(View view) {
-        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
-        hobbies.setVisibility(View.VISIBLE);
+        bio.setHobbies(String.format("Hobbies: %s",
+                binding.enterHobbies.getText().toString().trim()));
 
+//        binding.hobbiesText.setText(String.format("Hobbies: %s",
+//                binding.enterHobbies.getText().toString().trim()));
+
+        binding.invalidateAll();
+        binding.hobbiesText.setVisibility(View.VISIBLE);
+        // hide keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
